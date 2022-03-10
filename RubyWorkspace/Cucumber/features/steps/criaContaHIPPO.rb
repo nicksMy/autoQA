8f431c7edf903049e579188cd7d0ba8941450cb7
nicksMy <nicolas@duel.com.br>
2022-03-10 16:02:39 -0300

    continuidade hippo

---
+++ RubyWorkspace/Cucumber/features/steps/criaContaHIPPO.rb
@@ -0,0 +1,26 @@
+Dado('o site do Hippo') do
+end
+
+Quando('apresentar o botão Criar uma conta, clique') do
+end
+
+Quando('assim que exibir o alert para o CEP seguir com o preenchimento') do
+end
+
+Quando('quando mudar de tela completar todos os campos pessoa física') do
+end
+
+Quando('acionar o comando de cadastro de conta') do
+end
+
+Quando('quando mostrar o popup com a validação, clique') do
+end
+
+Quando('voltar para a homepage do Hippo') do
+end
+
+Quando('entrar na tela "Você bem +" e visualizar as três opções de ofertas e descontos') do
+end
+
+Então('fechar o Browser') do
+end
